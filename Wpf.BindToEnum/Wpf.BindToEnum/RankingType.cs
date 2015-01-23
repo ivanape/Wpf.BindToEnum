51f00f9882fe2f0f9038b31028428f6091354a97
unknown <E384465@VFC779.inetpsa.com>
2015-01-23 09:06:44 +0100

    Added sample source code.

---
+++ Wpf.BindToEnum/Wpf.BindToEnum/RankingType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf.BindToEnum
+{
+    public enum RankingType
+    {
+        [Description("Semana")]
+        Week,
+        [Description("Mes")]
+        Month,
+        [Description("Año")]
+        Year
+    }
+}
---
+++ Wpf.BindToEnum/Wpf.BindToEnum/RankingTypeToStringConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Wpf.BindToEnum
+{
+    public class RankingTypeToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (string.IsNullOrEmpty(value.ToString()))  
+                return RankingType.Month;
+
+            return (StringToEnum<RankingType>(value.ToString())).GetDescription(); 
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (string.IsNullOrEmpty(value.ToString())) 
+                return RankingType.Month;
+
+            return StringToEnum<RankingType>(value.ToString());
+        }
+
+        public static T StringToEnum<T>(string name)
+        {
+            return (T)Enum.Parse(typeof(T), name);
+        }
+
+
+    }
+}
